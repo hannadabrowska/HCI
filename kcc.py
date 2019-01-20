b4cdbf31962c322e79c0a8c0b598fc0c1aee0766
hannadabrowska <45164633+hannadabrowska@users.noreply.github.com>
2019-01-20 11:15:29 +0100

    zadanie 13

---
+++ kcc.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import aseegg as ag
+
+
+
+t = np.linspace(0, 37.9, 200*37.9)
+probkowanie=200
+dane = pd.read_csv(r"C:\Users\acer-komp\Desktop\kc\sub-01_trial-07.csv", delimiter=',', engine='python')
+sygnal = dane["kol2"]
+# plt.plot(t, sygnal )
+# plt.xlabel("Czas[s]")
+# plt.ylabel("Amplituda[-]")
+
+przefiltrowany = ag.pasmowozaporowy(sygnal, probkowanie, 49, 51)
+przefiltrowany2 = ag.pasmowoprzepustowy(przefiltrowany, probkowanie, 1, 50)
+
+plt.subplot(2, 1, 1)
+plt.plot(t, sygnal)
+# plt.xlim([0, 3])
+plt.xlabel('Czas [s]')
+plt.ylabel('Amplituda [-]')
+plt.subplot(2, 1, 2)
+plt.plot(t, przefiltrowany2)
+# plt.xlim([0, 3])
+plt.xlabel('Czas [s]')
+plt.ylabel('Amplituda [-]')
+plt.show()
